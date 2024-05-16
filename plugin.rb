308b075930ac82a16d67a57c53cdd5355243b10d
Loïc Guitaut <loic@discourse.org>
2024-05-16 16:33:28 +0200

    DEV: Update rubocop-discourse to latest version

--- plugin.rb
+++ plugin.rb
@@ -18,44 +18,9 @@ after_initialize do
     end
   end
 
-  require_dependency "invite"
-  class ::Invite
-    def self.generate_invite_tokens(invited_by, quantity = nil, group_names = nil)
-      invite_tokens = []
-      quantity ||= 1
-      group_ids = get_group_ids(group_names)
+  require_relative "lib/discourse_invite_tokens/invite_extension"
 
-      quantity.to_i.times do
-        invite =
-          Invite.create!(
-            invited_by: invited_by,
-            emailed_status: Invite.emailed_status_types[:not_required],
-          )
-        group_ids = group_ids - invite.invited_groups.pluck(:group_id)
-        group_ids.each { |group_id| invite.invited_groups.create!(group_id: group_id) }
-        invite_tokens.push(invite.invite_key)
-      end
-
-      invite_tokens
-    end
-
-    def self.redeem_from_token(token, email, username = nil, name = nil, topic_id = nil)
-      invite = Invite.find_by(invite_key: token)
-      if invite
-        lower_email = Email.downcase(email)
-        user = User.find_by_email(lower_email)
-        raise UserExists.new I18n.t("invite.user_exists_simple") if user.present?
-
-        if topic_id && Topic.find_by_id(topic_id) &&
-             !invite.topic_invites.pluck(:topic_id).include?(topic_id)
-          invite.topic_invites.create!(invite_id: invite.id, topic_id: topic_id)
-        end
-        user =
-          InviteRedeemer.new(invite: invite, email: email, username: username, name: name).redeem
-      end
-      user
-    end
-  end
+  reloadable_patch { Invite.prepend(DiscourseInviteTokens::InviteExtension) }
 
   require_dependency "application_controller"
   class DiscourseInviteTokens::InviteTokensController < ::ApplicationController
